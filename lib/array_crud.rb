109beb95e2dd6d2245d5cc49eb6eb7215a9aa34e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-18 18:33:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  new_array = []
+  this_array = []
 end
 
 def create_an_array
-  
+  that_array = [toot, burp, squeak]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshit(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array{-1}
 end
